86ccca86e875822cbd121286e9e23c3a1e1a6337
josepharunjoseph <prasanna@prasannas-mac-mini.local>
2017-03-29 18:22:14 +0530

    Initial Commit

---
+++ TestDatePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDatePicker
+//
+//  Created by prasanna on 3/29/17.
+//  Copyright © 2017 Ashadeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestDatePicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDatePicker
+//
+//  Created by prasanna on 3/29/17.
+//  Copyright © 2017 Ashadeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
